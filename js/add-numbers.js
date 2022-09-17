37213ab0a266e13dbed1e8496d41b7423b5106fd
towkir-turtle <towkir.ahmed603@gmail.com>
2022-09-17 14:07:58 +0600

    first commit

---
+++ js/add-numbers.js
@@ -0,0 +1,10 @@
+// const num1 = 12;
+// const num2 = 13;
+// const sum = num1 + num2;
+// console.log(sum);
+
+// Method-02:
+const number1 = parseInt(prompt("Enter the first number: "));
+const number2 = parseInt(prompt("Enter the second number: "));
+const total = number1 + number2;
+console.log(total);
